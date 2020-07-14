accb1df23ca82109f83dcd0907efb576951500b4
sally <https://github.com/youngjae99/CS496_MadCamp>
2020-07-14 17:34:04 +0900

    5:33

--- app/src/main/java/com/example/firstproject/Fragment3.java
+++ app/src/main/java/com/example/firstproject/Fragment3.java
@@ -71,16 +71,15 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
-import noman.googleplaces.NRPlaces;
-import noman.googleplaces.Place;
-import noman.googleplaces.PlaceType;
-import noman.googleplaces.PlacesException;
-import noman.googleplaces.PlacesListener;
+//import noman.googleplaces.NRPlaces;
+//import noman.googleplaces.Place;
+//import noman.googleplaces.PlaceType;
+//import noman.googleplaces.PlacesException;
+//import noman.googleplaces.PlacesListener;
 
 public class Fragment3  extends Fragment
         implements OnMapReadyCallback ,
-        ActivityCompat.OnRequestPermissionsResultCallback,
-        PlacesListener {
+        ActivityCompat.OnRequestPermissionsResultCallback{
 
     ////////////////////////////geocoding////////////////////////////
     Button addressButton;
@@ -493,7 +492,7 @@ public class Fragment3  extends Fragment
         mMap.addMarker(markerOptions);
 
         mMap.moveCamera(CameraUpdateFactory.newLatLng(SEOUL));
-        mMap.animateCamera(CameraUpdateFactory.zoomTo(10));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
     }
 
     LocationCallback locationCallback = new LocationCallback() {
@@ -690,7 +689,7 @@ public class Fragment3  extends Fragment
 
 
         //디폴트 위치, Seoul
-        LatLng DEFAULT_LOCATION = new LatLng(37.56, 126.97);
+        LatLng DEFAULT_LOCATION = new LatLng(36.372292, 127.360371);
         String markerTitle = "위치정보 가져올 수 없음";
         String markerSnippet = "위치 퍼미션과 GPS 활성 요부 확인하세요";
 
@@ -846,69 +845,69 @@ public class Fragment3  extends Fragment
         }
     }
 
-    @Override
-    public void onPlacesFailure(PlacesException e) {
+//    @Override
+//    public void onPlacesFailure(PlacesException e) {
+//
+//    }
 
-    }
+//    @Override
+//    public void onPlacesStart() {
+//
+//    }
 
-    @Override
-    public void onPlacesStart() {
+//    @Override
+//    public void onPlacesSuccess(final List<Place> places) {
+//        getActivity().runOnUiThread(new Runnable() {
+//            @Override
+//            public void run() {
+//                for (noman.googleplaces.Place place : places) {
+//
+//                    LatLng latLng
+//                            = new LatLng(place.getLatitude()
+//                            , place.getLongitude());
+//
+//                    String markerSnippet = getCurrentAddress(latLng);
+//
+//                    MarkerOptions markerOptions = new MarkerOptions();
+//                    markerOptions.position(latLng);
+//                    markerOptions.title(place.getName());
+//                    markerOptions.snippet(markerSnippet);
+//                    Marker item = mMap.addMarker(markerOptions);
+//                    previous_marker.add(item);
+//
+//                }
+//
+//                //중복 마커 제거
+//                HashSet<Marker> hashSet = new HashSet<Marker>();
+//                hashSet.addAll(previous_marker);
+//                previous_marker.clear();
+//                previous_marker.addAll(hashSet);
+//
+//            }
+//        });
+//    }
 
-    }
+//    @Override
+//    public void onPlacesFinished() {
+//
+//    }
 
-    @Override
-    public void onPlacesSuccess(final List<Place> places) {
-        getActivity().runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                for (noman.googleplaces.Place place : places) {
-
-                    LatLng latLng
-                            = new LatLng(place.getLatitude()
-                            , place.getLongitude());
-
-                    String markerSnippet = getCurrentAddress(latLng);
-
-                    MarkerOptions markerOptions = new MarkerOptions();
-                    markerOptions.position(latLng);
-                    markerOptions.title(place.getName());
-                    markerOptions.snippet(markerSnippet);
-                    Marker item = mMap.addMarker(markerOptions);
-                    previous_marker.add(item);
-
-                }
-
-                //중복 마커 제거
-                HashSet<Marker> hashSet = new HashSet<Marker>();
-                hashSet.addAll(previous_marker);
-                previous_marker.clear();
-                previous_marker.addAll(hashSet);
-
-            }
-        });
-    }
-
-    @Override
-    public void onPlacesFinished() {
-
-    }
-
-    public void showPlaceInformation(LatLng location)
-    {
-        mMap.clear();//지도 클리어
-
-        if (previous_marker != null)
-            previous_marker.clear();//지역정보 마커 클리어
-
-        new NRPlaces.Builder()
-                .listener(this)
-                .key("AIzaSyCVX6n_K-jNboEmw5wjOm-Q_mlfcp51Re8")
-                .latlng(location.latitude, location.longitude)//현재 위치
-                .radius(500) //500 미터 내에서 검색
-                .type(PlaceType.RESTAURANT) //음식점
-                .build()
-                .execute();
-    }
+//    public void showPlaceInformation(LatLng location)
+//    {
+//        mMap.clear();//지도 클리어
+//
+//        if (previous_marker != null)
+//            previous_marker.clear();//지역정보 마커 클리어
+//
+//        new NRPlaces.Builder()
+//                .listener(this)
+//                .key("AIzaSyCVX6n_K-jNboEmw5wjOm-Q_mlfcp51Re8")
+//                .latlng(location.latitude, location.longitude)//현재 위치
+//                .radius(500) //500 미터 내에서 검색
+//                .type(PlaceType.RESTAURANT) //음식점
+//                .build()
+//                .execute();
+//    }
 
     /*
     private void geoLocate(){
